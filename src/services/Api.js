fc66d3e08c4e263a504c6d14b05911a39daf4003
DarkteK <cs.gamegonzalo@gmail.com>
2019-09-20 18:07:13 -0300

    fix: update API due CORS restriction
    replace reddit js for top local js

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export default () => {
   return axios.create({
-    baseURL: "https://reddit.com",
     withCredentials: false,
     headers: {
       Accept: "application/json",
